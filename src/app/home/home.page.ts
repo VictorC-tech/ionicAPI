71651dd51e0e2a7756b8c02116b89418cbc1e2a8
Victor Cecilio <victor020gacha@gmail.com>
2024-09-18 07:28:29 -0300

    API

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  films: Observable<any> | undefined;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    public toastController: ToastController
+  ) {}
+
+  ngOnInit() {
+    this.films = this.http.get('https://swapi.dev/api/films').pipe(catchError(erro => this.exibirErro(erro)));
+  }
+
+  async exibirErro(erro) {
+    const toast = await this.toastController.create({
+      message: 'Erro ao consultar a API:' + erro.status + ':' + erro.message,
+      duration: 4000,
+      color: 'danger',
+      position: 'middle',
+    });
+    console.log(erro);
+    toast.present();
+    return null;
+  }
+
+  openDetails(film) {
+    let split = film.url.split('/');
+    let filmId = split[5];
+    this.router.navigateByUrl(`/filme-detalhe/${filmId}`);
+  }
+}
